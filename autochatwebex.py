d5559d1810e89c8cadf181c2813fa9f080a672c5
gabrielsidauruk <96155837+gabrielsidauruk@users.noreply.github.com>
2022-06-25 06:41:49 +0000

    Update autochatwebex.py

--- autochatwebex.py
+++ autochatwebex.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 import requests
 import json
 messages="Terima kasih sudah mengikut program kami. Silahkan hubungi admin WA kami untuk dimasukkan ke dalam grup !"
