f96d95379f64ce47fe0a4c604500323c322434cb
gabrielsidauruk <96155837+gabrielsidauruk@users.noreply.github.com>
2022-06-27 11:34:03 +0000

    Update autochatwebex.py

--- autochatwebex.py
+++ autochatwebex.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import requests
 import json
-messages="Tes dari git! yang ke 2"
+messages="Tes dari git! yang baru"
 url = 'https://webexapis.com/v1/messages'
 file=open("email.txt","r")
 data=[]
 for i in file:
     try:
-        access_token = 'YTAxMWYxYmYtYTlhNy00YmFiLThiZTktNTI2Y2NlYTI1YTk2YmNkZTQ4MDctOTM4_P0A1_296842f3-00c1-4128-b47d-4e1e1fd7d576'
+        access_token = 'ZTA4OTQzYWItZDMzNS00ZTRkLTkyZDItMDgwNzVjNGIxM2JlMDhmM2UyYWMtNGM5_P0A1_296842f3-00c1-4128-b47d-4e1e1fd7d576'
         i = i.replace("\n", "")
         data.append(i)
         headers={
